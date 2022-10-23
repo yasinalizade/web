d07e39d49457b50898173b7bd60e7909176ab67d
Yasin Alizade <yasin.alizade@yandex.ru>
2022-10-23 18:34:58 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
 sudo /etc/init.d/nginx restart
-
-# Проверка правильности выполнения задания
-curl http://localhost/uploads/test.txt
