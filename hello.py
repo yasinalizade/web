2386f69dd1df8cc3d9cc7fd3111c1f510bd080d4
Yasin Alizade <yasin.alizade@yandex.ru>
2022-10-23 18:30:29 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,5 @@
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
+  
